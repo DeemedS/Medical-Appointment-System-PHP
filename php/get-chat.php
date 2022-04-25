7f5b007448cb3e5466cd9de67e21fde54d6bed17
guianalankem <kem.guianalan@gmail.com>
2022-04-25 09:00:52 +0800

    Update get-chat.php

--- php/get-chat.php
+++ php/get-chat.php
@@ -1,6 +1,7 @@
 <?php 
     session_start();
     if(isset($_SESSION['login_id'])){
+
         include_once "../admin/db_connect.php";
         include('../src/components/database/userquery.php');
 
@@ -10,29 +11,99 @@
         if ($type == 2){
             $outgoing_id = $did;
         }
+
+
         
 
         $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
         $output = "";
-        $sql = "SELECT * FROM messages LEFT JOIN users ON users.id = messages.outgoing_msg_id
+
+        $sql = "SELECT * FROM messages LEFT JOIN users ON users.id = $uid
                 WHERE (outgoing_msg_id = {$outgoing_id} AND incoming_msg_id = {$incoming_id})
                 OR (outgoing_msg_id = {$incoming_id} AND incoming_msg_id = {$outgoing_id}) ORDER BY msg_id";
+
         $query = mysqli_query($conn, $sql);
         if(mysqli_num_rows($query) > 0){
             while($row = mysqli_fetch_assoc($query)){
                 if($row['outgoing_msg_id'] === $outgoing_id){
-                    $output .= '<div class="chat outgoing">
+
+                    if ($row['img_msg'] != null){
+
+                        if ($row['msg'] != null) {
+
+                            $output .= '<div class="chat outgoing">
                                 <div class="details">
-                                    <p>'. $row['msg'] .'</p>
+                                    <div class="imgmess">
+                                        <p>'. $row['msg'] .'</p>
+                                        <img src="assets/img/'. $row['img_msg'] .'" alt="" width="250" height="150">
+                                    </div>
                                 </div>
                                 </div>';
+                        }
+                    
+                        else {
+                            $output .= '<div class="chat outgoing">
+                                <div class="details">
+                                    <div class="imgmess">
+                                        <img src="assets/img/'. $row['img_msg'] .'" alt="" width="250" height="150">
+                                    </div>
+                                </div>
+                                </div>';
+                        }
+                        
+                    }
+
+                    else {
+
+                        $output .= '<div class="chat outgoing">
+                                <div class="details">
+                                    <p class="mess">'. $row['msg'] .'</p>
+                                </div>
+                                </div>';
+
+                    }
+
+
+                    
                 }else{
-                    $output .= '<div class="chat incoming">
-                                <img src="php/images/'.$row['img'].'" alt="">
-                                <div class="details">
+                    if ($row['img_msg'] != null){
+
+                        if ($row['msg'] != null) {
+
+                            $output .= '<div class="chat incoming">
+                            <div class="details">
+                                <div class="imgmess">
                                     <p>'. $row['msg'] .'</p>
+                                    <img src="assets/img/'. $row['img_msg'] .'" alt="" width="250" height="150">
                                 </div>
-                                </div>';
+                            </div>
+                            </div>';
+
+                        }
+
+                        else {
+
+                            $output .= '<div class="chat incoming">
+                            <div class="details">
+                                <div class="imgmess">
+                                    <img src="assets/img/'. $row['img_msg'] .'" alt="" width="250" height="150">
+                                </div>
+                            </div>
+                            </div>';
+                        }
+
+
+                    }
+
+                    else {
+
+                        $output .= '<div class="chat incoming">
+                        <div class="details">
+                            <p class="mess">'. $row['msg'] .'</p>
+                        </div>
+                        </div>';
+                    }
+                   
                 }
             }
         }else{
