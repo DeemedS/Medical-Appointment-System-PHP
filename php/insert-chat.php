75042327f2a30a847b19be684f98052c158f22c2
guianalankem <kem.guianalan@gmail.com>
2022-04-25 09:00:58 +0800

    Update insert-chat.php

--- php/insert-chat.php
+++ php/insert-chat.php
@@ -13,10 +13,44 @@
         
         $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
         $message = mysqli_real_escape_string($conn, $_POST['message']);
-        if(!empty($message)){
-            $sql = mysqli_query($conn, "INSERT INTO messages (incoming_msg_id, outgoing_msg_id, msg)
-                                        VALUES ({$incoming_id}, {$outgoing_id}, '{$message}')") or die();
+
+        if(isset($_FILES['img_msg'])){
+
+            $img_name = $_FILES['img_msg']['name'];
+            $img_type = $_FILES['img_msg']['type'];
+            $tmp_name = $_FILES['img_msg']['tmp_name'];
+                    
+            $img_explode = explode('.',$img_name);
+            $img_ext = end($img_explode);
+
+            $extensions = ["jpeg", "png", "jpg"];
+
+            if(in_array($img_ext, $extensions) === true){
+                $types = ["image/jpeg", "image/jpg", "image/png"];
+                    if(in_array($img_type, $types) === true){
+                        $time = time();
+                        $new_img_name = $time.$img_name;
+
+                        if(move_uploaded_file($tmp_name,"../assets/img/".$new_img_name)){
+                            $ran_id = rand(time(), 100000000);
+
+                                $sql = mysqli_query($conn, "INSERT INTO messages (incoming_msg_id, outgoing_msg_id, msg, img_msg)
+                                VALUES ({$incoming_id}, {$outgoing_id}, '{$message}', '{$new_img_name}')") or die();
+                        }
+                    }
+            }
         }
+
+        if ($img_name == ""){
+
+            if(!empty($message)) {
+                $sql = mysqli_query($conn, "INSERT INTO messages (incoming_msg_id, outgoing_msg_id, msg)
+                                            VALUES ({$incoming_id}, {$outgoing_id}, '{$message}')") or die();
+            }
+        }
+
+
+
     }else{
         header("location: ../login.php");
     }
