062ae15f3cd60dcc30bf4905be7588c366295a25
guianalankem <kem.guianalan@gmail.com>
2022-04-28 00:11:02 +0800

    Update chat.php

--- chat.php
+++ chat.php
@@ -41,22 +41,68 @@
         <div class="messages">
 
         </div>
+
+      <div class="img-up">
+        <div id="close">
+          <button onclick="closeimage();"><i class="material-icons">close</i></button>
+          <img  id='blah' src="" alt=""/>
+        </div>
+      </div>
+
       </div>
         <form action="#" class="typing-area">
           <input type="text" class="incoming_id" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
           <input type="text" name="message" class="input-field" placeholder="Type a message here..." autocomplete="off">
 
             <label for="file-input"><i class="large material-icons">image</i></label>
-            <input id="file-input" type="file" name="img_msg" accept="image/x-png,image/gif,image/jpeg,image/jpg">
+            <input id="file-input" type="file" name="img_msg" accept="image/x-png,image/gif,image/jpeg,image/jpg" onchange="readURL(this);">
 
-
-          <button><i class="fab fa-telegram-plane"></i></button>
+          <button class="active"><i class="fab fa-telegram-plane"></i></button>
         </form>
 
     </section>
   </div>
 
   <script src="js/chat.js"></script>
+  <script>
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#blah').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    function closeimage(){
+      $('.img-up  ').hide()
+      const $blah = $('#blah');
+      $($blah).attr('src', '');
+      inputFile.value = "";
+    }
+
+
+    setInterval(function(){ 
+
+        const img = document.getElementById('blah');
+        const src = img.getAttribute('src');
+
+        if (src != ""){
+          $('.img-up  ').show()
+        }
+
+
+    }, 1000);
+
+
+
+    
+
+</script>
 
 </body>
 </html>
