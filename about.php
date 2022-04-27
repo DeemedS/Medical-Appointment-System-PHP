acf26990d529b654db4bba3cc2b7965a040ea1a2
guianalankem <kem.guianalan@gmail.com>
2022-04-27 20:53:23 +0800

    Update about.php

--- about.php
+++ about.php
@@ -1,19 +1,13 @@
- <!-- Masthead-->
-        <header class="masthead">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center justify-content-center text-center">
-                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
-                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
-                        <hr class="divider my-4" />
-                    </div>
-                    
-                </div>
-            </div>
-        </header>
 
-    <section class="page-section">
-        <div class="container">
-    <?php echo html_entity_decode($_SESSION['setting_about_content']) ?>        
-            
-        </div>
-        </section>
+<section class="page-section">
+    <div class="about title">
+        <h3>ABOUT US</h3>
+    </div>
+
+    <div class="container about-container">
+        <div class="about-content">
+                <?php echo html_entity_decode($_SESSION['setting_about_content']) ?>  
+        </div>    
+    </div>
+
+</section>
