04b30adcfa492f8ec734579d6712dd8d93c53d7b
guianalankem <kem.guianalan@gmail.com>
2022-04-29 23:45:20 +0800

    Update appointments.php

--- admin/appointments.php
+++ admin/appointments.php
@@ -10,58 +10,67 @@
 		$p_arr[$row['id']] = $row;
 	}
 ?>
-<div class="container-fluid">
-	<div class="col-md-12">
-		<div class="card">
-			<div class="card-body">
-				<button class="btn-primary btn btn-sm" type="button" id="new_appointment"><i class="fa fa-plus"></i> New Appointment</button>
-				<br>
-				<table class="table table-bordered">
-					<thead>
+
+<section class="appointments">
+
+	<div class="header-container">
+	<h3>APPOINTMENTS</h3>
+	</div>
+
+	<div class="container-fluid">
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-body">
+					<button class="btn-primary btn btn-sm" type="button" id="new_appointment"><i class="fa fa-plus"></i> New Appointment</button>
+					<br>
+					<table class="table table-bordered">
+						<thead>
+							<tr>
+							<th>Schedule</th>
+							<th>Doctor</th>
+							<th>Pateint</th>
+							<th>Status</th>
+							<th>Action</th>
+						</tr>
+						</thead>
+						<?php 
+						$where = '';
+						if($_SESSION['login_type'] == 2)
+							$where = " where doctor_id = ".$_SESSION['login_doctor_id'];
+						$qry = $conn->query("SELECT * FROM appointment_list ".$where." order by id desc ");
+						while($row = $qry->fetch_assoc()):
+						?>
 						<tr>
-						<th>Schedule</th>
-						<th>Doctor</th>
-						<th>Pateint</th>
-						<th>Status</th>
-						<th>Action</th>
-					</tr>
-					</thead>
-					<?php 
-					$where = '';
-					if($_SESSION['login_type'] == 2)
-						$where = " where doctor_id = ".$_SESSION['login_doctor_id'];
-					$qry = $conn->query("SELECT * FROM appointment_list ".$where." order by id desc ");
-					while($row = $qry->fetch_assoc()):
-					?>
-					<tr>
-						<td><?php echo date("l M d, Y h:i A",strtotime($row['schedule'])) ?></td>
-						<td><?php echo "DR. ".$doc_arr[$row['doctor_id']]['name'].', '.$doc_arr[$row['doctor_id']]['name'] ?></td>
-						<td><?php echo $p_arr[$row['patient_id']]['name'] ?></td>
-						<td>
-							<?php if($row['status'] == 0): ?>
-								<span class="badge badge-warning">Pending Request</span>
-							<?php endif ?>
-							<?php if($row['status'] == 1): ?>
-								<span class="badge badge-primary">Confirmed</span>
-							<?php endif ?>
-							<?php if($row['status'] == 2): ?>
-								<span class="badge badge-info">Rescheduled</span>
-							<?php endif ?>
-							<?php if($row['status'] == 3): ?>
-								<span class="badge badge-info">Done</span>
-							<?php endif ?>
-						</td>
-						<td class="text-center">
-							<button  class="btn btn-primary btn-sm update_app" type="button" data-id="<?php echo $row['id'] ?>">Update</button>
-							<button  class="btn btn-danger btn-sm delete_app" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
-						</td>
-					</tr>
-				<?php endwhile; ?>
-				</table>
+							<td><?php echo date("l M d, Y h:i A",strtotime($row['schedule'])) ?></td>
+							<td><?php echo "DR. ".$doc_arr[$row['doctor_id']]['name']?></td>
+							<td><?php echo $p_arr[$row['patient_id']]['name'] ?></td>
+							<td>
+								<?php if($row['status'] == 0): ?>
+									<span class="badge bg-warning">Pending Request</span>
+								<?php endif ?>
+								<?php if($row['status'] == 1): ?>
+									<span class="badge bg-primary">Confirmed</span>
+								<?php endif ?>
+								<?php if($row['status'] == 2): ?>
+									<span class="badge bg-info">Rescheduled</span>
+								<?php endif ?>
+								<?php if($row['status'] == 3): ?>
+									<span class="badge bg-info">Done</span>
+								<?php endif ?>
+							</td>
+							<td class="text-center">
+								<button  class="btn btn-primary btn-sm update_app" type="button" data-id="<?php echo $row['id'] ?>">Update</button>
+								<button  class="btn btn-danger btn-sm delete_app" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
+							</td>
+						</tr>
+					<?php endwhile; ?>
+					</table>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</section>
+
 <script>
 	$('.update_app').click(function(){
 		uni_modal("Edit Appintment","set_appointment.php?id="+$(this).attr('data-id'),"mid-large")
