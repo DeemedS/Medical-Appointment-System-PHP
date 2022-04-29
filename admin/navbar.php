240faa51879ea0cf8d7495d9887c49bae10ad151
guianalankem <kem.guianalan@gmail.com>
2022-04-29 23:45:54 +0800

    Update navbar.php

--- admin/navbar.php
+++ admin/navbar.php
@@ -1,24 +1,26 @@
-
-<style>
-</style>
-<nav id="sidebar" class='mx-lt-5 bg-dark' >
-		
-		<div class="sidebar-list">
-
-				<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-home"></i></span> Home</a>
-				<a href="index.php?page=appointments" class="nav-item nav-appointments"><span class='icon-field'><i class="fa fa-calendar"></i></span> Appointments</a>
-				<a href="index.php?page=doctors" class="nav-item nav-doctors"><span class='icon-field'><i class="fa fa-user-md"></i></span> Doctors</a>
-				<a href="index.php?page=categories" class="nav-item nav-categories"><span class='icon-field'><i class="fa fa-book-medical"></i></span> Medical Specialties</a>			
-				<?php if($_SESSION['login_type'] == 1): ?>
-				<a href="index.php?page=users" class="nav-item nav-users"><span class='icon-field'><i class="fa fa-users"></i></span> Users</a>
-				<a href="index.php?page=site_settings" class="nav-item nav-site_settings"><span class='icon-field'><i class="fa fa-cog"></i></span> Site Settings</a>
-			<?php endif; ?>
-		</div>
-
-</nav>
+<div class="navc">
+    <div class="l-navbar" id="nav-bar">
+        <nav class="nav">
+            <div> 
+                <div class="nav_list"> 
+					<a href="index.php?page=home" class="nav_link nav-home"><i class='bx bx-home-alt-2 nav_icon'></i></i> <span class="nav_name">Home</span> </a> 
+					<a href="index.php?page=appointments" class="nav_link nav-appointments"> <i class='bx bx-detail nav_icon'></i> <span class="nav_name">Appointments</span> </a> 
+					<a href="index.php?page=doctors" class="nav_link nav-doctors" > <i class='bx bxs-user-badge nav_icon'></i> <span class="nav_name">Doctors</span></a> 
+					<a href="index.php?page=categories" class="nav_link nav-categories"> <i class='bx bx-category nav_icon'></i> <span class="nav_name">Categories</span> </a> 
+					<a href="index.php?page=users" class="nav_link nav-users"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a> 
+					<a href="index.php?page=site_settings" class="nav_link site_settings"> <i class='bx bx-cog nav_icon'></i> <span class="nav_name">Site Settings</span> </a> 
+				</div>
+            </div> 
+			<a href="ajax.php?action=logout" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
+        </nav>
+    </div>
+<nav>
+	
 <script>
 	$('.nav-<?php echo isset($_GET['page']) ? $_GET['page'] : '' ?>').addClass('active')
 </script>
+
+<script src="assets/js/navbar.js"></script>
 <?php if($_SESSION['login_type'] == 2): ?>
 	<style>
 		.nav-sales ,.nav-users,.nav-doctors,.nav-categories{
@@ -26,3 +28,6 @@
 		}
 	</style>
 <?php endif ?>
+
+
+
