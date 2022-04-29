208478e50a6f969280791eee17d83095a5383263
guianalankem <kem.guianalan@gmail.com>
2022-04-29 23:45:36 +0800

    Update categories.php

--- admin/categories.php
+++ admin/categories.php
@@ -1,88 +1,95 @@
 <?php include('db_connect.php');?>
 
-<div class="container-fluid">
-	
-	<div class="col-lg-12">
-		<div class="row">
-			<!-- FORM Panel -->
-			<div class="col-md-4">
-			<form action="" id="manage-category">
-				<div class="card">
-					<div class="card-header">
-						    Medical Specialties Form
-				  	</div>
-					<div class="card-body">
-							<input type="hidden" name="id">
-							<div class="form-group">
-								<label class="control-label">Specialty</label>
-								<textarea name="name" id="" cols="30" rows="2" class="form-control"></textarea>
-							</div>
-							<div class="form-group">
-								<label for="" class="control-label">Image</label>
-								<input type="file" class="form-control" name="img" onchange="displayImg(this,$(this))">
-							</div>
-							<div class="form-group">
-								<img src="" alt="" id="cimg">
-							</div>	
-							
-					</div>
-							
-					<div class="card-footer">
-						<div class="row">
-							<div class="col-md-12">
-								<button class="btn btn-sm btn-primary col-sm-3 offset-md-3"> Save</button>
-								<button class="btn btn-sm btn-default col-sm-3" type="button" onclick="_reset()"> Cancel</button>
+<section class="categories">
+
+<div class="header-container">
+	<h3>CATEGORIES</h3>
+</div>
+	<div class="container-fluid">
+		
+		<div class="col-lg-12">
+			<div class="row">
+				<!-- FORM Panel -->
+				<div class="col-md-4">
+				<form action="" id="manage-category">
+					<div class="card">
+						<div class="card-header">
+								Medical Specialties Form
+						</div>
+						<div class="card-body">
+								<input type="hidden" name="id">
+								<div class="form-group">
+									<label class="control-label">Specialty</label>
+									<textarea name="name" id="" cols="30" rows="2" class="form-control"></textarea>
+								</div>
+								<div class="form-group">
+									<label for="" class="control-label">Image</label>
+									<input type="file" class="form-control" name="img" onchange="displayImg(this,$(this))">
+								</div>
+								<div class="form-group">
+									<img src="" alt="" id="cimg">
+								</div>	
+								
+						</div>
+								
+						<div class="card-footer">
+							<div class="row">
+								<div class="col-md-12">
+									<button class="btn btn-sm btn-primary col-sm-3 offset-md-3"> Save</button>
+									<button class="btn btn-sm btn-default col-sm-3" type="button" onclick="_reset()"> Cancel</button>
+								</div>
 							</div>
 						</div>
 					</div>
+				</form>
 				</div>
-			</form>
-			</div>
-			<!-- FORM Panel -->
+				<!-- FORM Panel -->
 
-			<!-- Table Panel -->
-			<div class="col-md-8">
-				<div class="card">
-					<div class="card-body">
-						<table class="table table-bordered table-hover">
-							<thead>
-								<tr>
-									<th class="text-center">#</th>
-									<th class="text-center">Image</th>
-									<th class="text-center">Name</th>
-									<th class="text-center">Action</th>
-								</tr>
-							</thead>
-							<tbody>
-								<?php 
-								$i = 1;
-								$cats = $conn->query("SELECT * FROM medical_specialty order by id asc");
-								while($row=$cats->fetch_assoc()):
-								?>
-								<tr>
-									<td class="text-center"><?php echo $i++ ?></td>
-									<td class="text-center">
-										<img src="../assets/img/<?php echo $row['img_path'] ?>" alt="">
-									</td>
-									<td class="">
-										 <b><?php echo $row['name'] ?></b>
-									</td>
-									<td class="text-center">
-										<button class="btn btn-sm btn-primary edit_cat" type="button" data-id="<?php echo $row['id'] ?>" data-name="<?php echo $row['name'] ?>" data-img_path="<?php echo $row['img_path'] ?>" >Edit</button>
-										<button class="btn btn-sm btn-danger delete_cat" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
-									</td>
-								</tr>
-								<?php endwhile; ?>
-							</tbody>
-						</table>
+				<!-- Table Panel -->
+				<div class="col-md-8">
+					<div class="card">
+						<div class="card-body">
+							<table class="table table-bordered table-hover">
+								<thead>
+									<tr>
+										<th class="text-center">#</th>
+										<th class="text-center">Image</th>
+										<th class="text-center">Name</th>
+										<th class="text-center">Action</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php 
+									$i = 1;
+									$cats = $conn->query("SELECT * FROM medical_specialty order by id asc");
+									while($row=$cats->fetch_assoc()):
+									?>
+									<tr>
+										<td class="text-center"><?php echo $i++ ?></td>
+										<td class="text-center">
+											<img src="../assets/img/<?php echo $row['img_path'] ?>" alt="">
+										</td>
+										<td class="">
+											<b><?php echo $row['name'] ?></b>
+										</td>
+										<td class="text-center">
+											<button class="btn btn-sm btn-primary edit_cat" type="button" data-id="<?php echo $row['id'] ?>" data-name="<?php echo $row['name'] ?>" data-img_path="<?php echo $row['img_path'] ?>" >Edit</button>
+											<button class="btn btn-sm btn-danger delete_cat" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
+										</td>
+									</tr>
+									<?php endwhile; ?>
+								</tbody>
+							</table>
+						</div>
 					</div>
 				</div>
+				<!-- Table Panel -->
 			</div>
-			<!-- Table Panel -->
-		</div>
-	</div>	
+		</div>	
 
-</div>
+	</div>
+<section>
+	
 <style>
 	
 	td{
