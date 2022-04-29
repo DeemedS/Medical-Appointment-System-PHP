3819ac046d8feab0a71570704667f023c3b3f21a
guianalankem <kem.guianalan@gmail.com>
2022-04-29 23:45:44 +0800

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -1,33 +1,32 @@
-<style>
-   
-</style>
+<section class="home">
+	<div class="container-fluid">
 
-<div class="containe-fluid">
-
-	<div class="row">
-		<div class="col-lg-12">
-			
-		</div>
-	</div>
-
-	<div class="row mt-3 ml-3 mr-3">
+		<div class="row">
 			<div class="col-lg-12">
-			<div class="card">
-				<div class="card-body">
-				<?php echo "Welcome back ".($_SESSION['login_type'] == 3 ? "Dr. ".$_SESSION['login_name'].','.$_SESSION['login_name_pref'] : $_SESSION['login_name'])."!"  ?>
-									
-				</div>
-				<hr>
-				<div class="row">
 				
+			</div>
+		</div>
+
+		<div class="row mt-3 ml-3 mr-3">
+			<div class="col-lg-12">
+				<div class="card">
+
+					<div class="card-body">
+					<?php echo "Welcome back ".($_SESSION['login_type'] == 3 ? "Dr. ".$_SESSION['login_name'].','.$_SESSION['login_name_pref'] : $_SESSION['login_name'])."!"  ?>			
+					</div>
+					<hr>
+
+					<div class="row">
+					</div>
+
+					</div>
 				</div>
 			</div>
-			
 		</div>
-		</div>
-	</div>
 
-</div>
+	</div>
+</section>
+
 <script>
 	
 </script>
